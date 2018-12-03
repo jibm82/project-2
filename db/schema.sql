94e8901d1532d2ccdab23e9d3657f92652953375
Jorge Baez <jibm_82@yahoo.com.mx>
2018-12-03 10:37:34 -0600

    Refactored sql files

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS bloodmap;
+CREATE DATABASE bloodmap;
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO BloodTypes (name, createdAt, updatedAt) VALUES 
+    ('O negative', NOW(), NOW()),
+    ('O positive', NOW(), NOW()),
+    ('A negative', NOW(), NOW()),
+    ('A positive', NOW(), NOW()),
+    ('B negative', NOW(), NOW()),
+    ('B positive', NOW(), NOW()),
+    ('AB negative', NOW(), NOW()),
+    ('AB positive', NOW(), NOW())
