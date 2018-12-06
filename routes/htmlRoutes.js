b6b4d677b292b048bc27e0146f83aadf83fd925c
eduardo moran <eduardo.moran.davalos@gmail.com>
2018-12-06 12:19:36 +0600

    handlebars + html routes + 404 error page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,30 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.bloodmap.findAll({}).then(function(dbbloodmap) {
+    db.User.findAll({}).then(function(dbbloodmap) {
+      console.log("___________dbbloodmap____________");
+      console.log(dbbloodmap.length);
+      console.log("___________dbbloodmap____________");
       res.render("index", {
         msg: "Welcome!",
-        examples: dbbloodmap
+        examples: [
+          {
+            id: 001,
+            text: Valeria
+          },
+          {
+            id: 002,
+            text: Jorge
+          },
+          {
+            id: 003,
+            text: Pablo
+          },
+          {
+            id: 004,
+            text: Eduardo
+          }
+        ]
       });
     });
   });
