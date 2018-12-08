e4a4c23eee54d38cc4b3fa4a56fea4f5c176f3e1
gerardo dubcovsky <pablodubco@gmail.com>
2018-12-07 19:39:50 -0600

    user profile front end and models

---
+++ models/Messages.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  var Messages = sequelize.define("Messages", {
+    body: DataTypes.STRING,
+    date: DataTypes.DATE,
+    href: DataTypes.STRING
+  });
+
+  Messages.associate = function(models) {
+    Messages.belongsTo(models.User);
+    Messages.hasOne(models.DonorProfile);
+    Messages.hasOne(models.Request);
+  };
+  return Messages;
+};
