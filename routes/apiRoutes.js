10d51c2daa03560e07042f95248355ee47f05c5a
Jorge Baez <jibm_82@yahoo.com.mx>
2018-12-08 02:31:33 -0600

    Implemented blood request notifications

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,35 +3,31 @@ var jobs = require("../tools/jobs");
 var geocoder = require("../tools/geocoder");
 var signupValidation = require("../validations/signupValidation");
 
+function sendRequestNotifications(bloodRequestId) {
+  db.BloodRequest.findById(bloodRequestId).then(function(BloodRequest) {
+    var long = BloodRequest.location.coordinates[0];
+    var lat = BloodRequest.location.coordinates[1];
+    db.DonorProfile.getAround(lat, long, BloodRequest.BloodTypeId).then(
+      function(donorProfiles) {
+        donorProfiles.forEach(function(donorProfile, index) {
+          jobs
+            .create("sendNotification", {
+              userId: donorProfile.UserId,
+              bloodRequestId: BloodRequest.id
+            })
+            .delay(1000 * index)
+            .events(false)
+            .save();
+        });
+      }
+    );
+  });
+}
+
 module.exports = function(app, passport) {
   // Get all bloodtypes
   app.get("/api/bloodtypes", function(req, res) {
     db.BloodType.findAll({}).then(function(dbBloodmap) {
-      res.json(dbBloodmap);
-    });
-  });
-
-  // Get all users
-  app.get("/api/users", function(req, res) {
-    db.User.findAll({}).then(function(dbBloodmap) {
-      res.json(dbBloodmap);
-    });
-  });
-
-  app.get("/api/donors", function(req, res) {
-    db.User.findAll({
-      include: [
-        {
-          model: db.DonorProfile,
-          required: true,
-          include: [
-            {
-              model: db.BloodType
-            }
-          ]
-        }
-      ]
-    }).then(function(dbBloodmap) {
       res.json(dbBloodmap);
     });
   });
@@ -66,43 +62,18 @@ module.exports = function(app, passport) {
           if (err) {
             return next(err);
           }
+          if (req.body.donor === null) {
+            sendRequestNotifications(user.BloodRequests[0].id);
+          }
           return res.json({ result: true });
         });
       })(req, res, next);
     }
   });
 
-  app.get("/api/nearbydonors/:id", function(req, res) {
-    db.BloodRequest.findById(req.params.id).then(function(BloodRequest) {
-      var long = BloodRequest.location.coordinates[0];
-      var lat = BloodRequest.location.coordinates[1];
-      db.DonorProfile.getAround(lat, long, BloodRequest.BloodTypeId).then(
-        function(donors) {
-          res.json(donors);
-        }
-      );
-    });
-  });
-
   app.post("/api/geocode/", function(req, res) {
     geocoder.geocode(req.body.address, function(results) {
       res.json(results);
-    });
-  });
-
-  app.get("/api/send-notifications", function(req, res) {
-    db.User.findAll({}).then(function(users) {
-      users.forEach(function(user, index) {
-        jobs
-          .create("sendNotification", {
-            email: user.email
-          })
-          .delay(1000 * index)
-          .events(false)
-          .save();
-      });
-
-      res.json({ result: true, deliveries: users.length });
     });
   });
 };
--- tools/mailer.js
+++ tools/mailer.js
@@ -8,6 +8,19 @@ var mailer = {
       text:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id finibus nibh. Cras vitae aliquet enim. Vivamus vel suscipit leo. Duis sed porta augue, facilisis eleifend mauris. Morbi in maximus neque. Mauris ac dolor dui. Nullam varius justo ut turpis aliquam, condimentum iaculis leo porttitor. Morbi arcu tellus, placerat a velit nec, fringilla efficitur ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     });
+  },
+
+  newRequest: function(bloodRequest, user) {
+    return transporter.sendMail({
+      from: "'Bloodmap' <request@bloodmap.com>",
+      to: user.email,
+      subject: "New Blood Request #" + bloodRequest.id,
+      text:
+        "Hello " +
+        user.name +
+        " there is a blood request near you at " +
+        bloodRequest.address
+    });
   }
 };
 
